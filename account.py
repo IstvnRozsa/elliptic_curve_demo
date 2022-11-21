52514e88e52bc4aae37f6e9a8141d3a6ac31da10
pistirozsa <pisti.rzsa@gmail.com>
2022-11-21 10:31:42 +0100

    remove const 3

--- account.py
+++ account.py
@@ -4,7 +4,7 @@ class Account:
     def __init__(self, name:str, secret_key:int, ecurve:Curve):
         self.name = name
         self.secret_key = secret_key
-        self.public_key = ecurve.double_and_add(3, ecurve.generator)
+        self.public_key = ecurve.double_and_add(self.secret_key, ecurve.generator)
 
     def __str__(self) -> str:
         return "Name: " + self.name + ", Secret key: " + str(self.secret_key) + " Public key: " + str(self.public_key)
